800866aa596a1ae4866fd9bbab592c6099eb2ecf
Shimizu Naoto <chokutto@yahoo.co.jp>
2013-10-31 23:59:28 +0900

    Geom: define Vector2f class.

--- include/geom.h
+++ include/geom.h
@@ -1,1 +1,22 @@
 #pragma once
+
+namespace Geom {
+
+#pragma warning(push)
+#pragma warning(disable: 4201)
+
+struct Vector2f {
+	union {
+		float v[2];
+		struct {
+			float x;
+			float y;
+		};
+	};
+};
+
+#pragma warning(pop)
+
+
+
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,18 @@
 #include <gtest/gtest.h>
 #include <geom.h>
 
+TEST(GeomVecter2fTest,AccessXY)
+{
+	Geom::Vector2f vec = { 1.0, 2.0 };
+	EXPECT_EQ(1.0, vec.x);
+	EXPECT_EQ(2.0, vec.y);
+}
+
+TEST(GeomVecter2fTest,AccessWithIndices)
+{
+	Geom::Vector2f vec = { 1.0, 2.0 };
+	EXPECT_EQ(1.0, vec.v[0]);
+	EXPECT_EQ(2.0, vec.v[1]);
+}
+
+
