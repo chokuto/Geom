d928f45de7782f3f1b99bd8effa1764fa129feb0
Shimizu Naoto <chokutto@yahoo.co.jp>
2015-05-06 10:07:55 +0900

    GeomVector: Change class name from Geom::Vector* to CGeomVector*.

--- include/geom.h
+++ include/geom.h
@@ -1,13 +1,11 @@
 #pragma once
 #include <cmath>
 
-namespace Geom {
-
 #pragma warning(push)
 #pragma warning(disable: 4201)
 
 template<typename T>
-struct Vector2T {
+struct CGeomVector2T {
 	union {
 		T v[2];
 		struct {
@@ -16,48 +14,48 @@ struct Vector2T {
 		};
 	};
 
-	Vector2T() {}
-	Vector2T(T x0, T y0) : x(x0), y(y0) {}
+	CGeomVector2T() {}
+	CGeomVector2T(T x0, T y0) : x(x0), y(y0) {}
 
 	T Length() const;
 	T LengthSquared() const;
 
-	Vector2T operator+() const;
-	Vector2T operator-() const;
-	Vector2T& operator+=(const Vector2T& vec);
-	Vector2T& operator-=(const Vector2T& vec);
-	Vector2T& operator*=(T multiplier);
-	Vector2T& operator/=(T divisor);
+	CGeomVector2T operator+() const;
+	CGeomVector2T operator-() const;
+	CGeomVector2T& operator+=(const CGeomVector2T& vec);
+	CGeomVector2T& operator-=(const CGeomVector2T& vec);
+	CGeomVector2T& operator*=(T multiplier);
+	CGeomVector2T& operator/=(T divisor);
 };
 
 #pragma warning(pop)
 
 template<typename T>
-T Vector2T<T>::LengthSquared() const
+T CGeomVector2T<T>::LengthSquared() const
 {
 	return (x * x) + (y * y);
 }
 
 template<typename T>
-T Vector2T<T>::Length() const
+T CGeomVector2T<T>::Length() const
 {
 	return std::sqrt(LengthSquared());
 }
 
 template<typename T>
-Vector2T<T> Vector2T<T>::operator+() const
+CGeomVector2T<T> CGeomVector2T<T>::operator+() const
 {
 	return *this;
 }
 
 template<typename T>
-Vector2T<T> Vector2T<T>::operator-() const
+CGeomVector2T<T> CGeomVector2T<T>::operator-() const
 {
-	return Vector2T<T>(-x, -y);
+	return CGeomVector2T<T>(-x, -y);
 }
 
 template<typename T>
-Vector2T<T>& Vector2T<T>::operator+=(const Vector2T<T>& vec)
+CGeomVector2T<T>& CGeomVector2T<T>::operator+=(const CGeomVector2T<T>& vec)
 {
 	x += vec.x;
 	y += vec.y;
@@ -65,7 +63,7 @@ Vector2T<T>& Vector2T<T>::operator+=(const Vector2T<T>& vec)
 }
 
 template<typename T>
-Vector2T<T>& Vector2T<T>::operator-=(const Vector2T<T>& vec)
+CGeomVector2T<T>& CGeomVector2T<T>::operator-=(const CGeomVector2T<T>& vec)
 {
 	x -= vec.x;
 	y -= vec.y;
@@ -73,7 +71,7 @@ Vector2T<T>& Vector2T<T>::operator-=(const Vector2T<T>& vec)
 }
 
 template<typename T>
-Vector2T<T>& Vector2T<T>::operator*=(T multiplier)
+CGeomVector2T<T>& CGeomVector2T<T>::operator*=(T multiplier)
 {
 	x *= multiplier;
 	y *= multiplier;
@@ -81,7 +79,7 @@ Vector2T<T>& Vector2T<T>::operator*=(T multiplier)
 }
 
 template<typename T>
-Vector2T<T>& Vector2T<T>::operator/=(T divisor)
+CGeomVector2T<T>& CGeomVector2T<T>::operator/=(T divisor)
 {
 	x /= divisor;
 	y /= divisor;
@@ -89,70 +87,69 @@ Vector2T<T>& Vector2T<T>::operator/=(T divisor)
 }
 
 template<typename T>
-Vector2T<T> operator+(const Vector2T<T>& vec1, const Vector2T<T>& vec2)
+CGeomVector2T<T> operator+(const CGeomVector2T<T>& vec1, const CGeomVector2T<T>& vec2)
 {
-	Vector2T<T> result(vec1);
+	CGeomVector2T<T> result(vec1);
 	result += vec2;
 	return result;
 }
 
 template<typename T>
-Vector2T<T> operator-(const Vector2T<T>& vec1, const Vector2T<T>& vec2)
+CGeomVector2T<T> operator-(const CGeomVector2T<T>& vec1, const CGeomVector2T<T>& vec2)
 {
-	Vector2T<T> result(vec1);
+	CGeomVector2T<T> result(vec1);
 	result -= vec2;
 	return result;
 }
 
 template<typename T1, typename T2>
-Vector2T<T1> operator*(const Vector2T<T1>& vec, T2 multiplier)
+CGeomVector2T<T1> operator*(const CGeomVector2T<T1>& vec, T2 multiplier)
 {
-	Vector2T<T1> result(vec);
+	CGeomVector2T<T1> result(vec);
 	result *= multiplier;
 	return result;
 }
 
 template<typename T1, typename T2>
-Vector2T<T2> operator*(T1 multiplier, const Vector2T<T2>& vec)
+CGeomVector2T<T2> operator*(T1 multiplier, const CGeomVector2T<T2>& vec)
 {
-	Vector2T<T2> result(vec);
+	CGeomVector2T<T2> result(vec);
 	result *= multiplier;
 	return result;
 }
 
 template<typename T1, typename T2>
-Vector2T<T1> operator/(const Vector2T<T1>& vec, T2 multiplier)
+CGeomVector2T<T1> operator/(const CGeomVector2T<T1>& vec, T2 multiplier)
 {
-	Vector2T<T1> result(vec);
+	CGeomVector2T<T1> result(vec);
 	result /= multiplier;
 	return result;
 }
 
 template<typename T>
-bool operator==(const Vector2T<T>& vec1, const Vector2T<T>& vec2)
+bool operator==(const CGeomVector2T<T>& vec1, const CGeomVector2T<T>& vec2)
 {
 	return (vec1.x == vec2.x) && (vec1.y == vec2.y);
 }
 
 template<typename T>
-bool operator!=(const Vector2T<T>& vec1, const Vector2T<T>& vec2)
+bool operator!=(const CGeomVector2T<T>& vec1, const CGeomVector2T<T>& vec2)
 {
 	return !(vec1 == vec2);
 }
 
 template<typename T>
-T DotProduct(const Vector2T<T>& vec1, const Vector2T<T>& vec2)
+T DotProduct(const CGeomVector2T<T>& vec1, const CGeomVector2T<T>& vec2)
 {
 	return (vec1.x * vec2.x) + (vec1.y * vec2.y);
 }
 
 template<typename T>
-T CrossProduct2D(const Vector2T<T>& vec1, const Vector2T<T>& vec2)
+T CrossProduct2D(const CGeomVector2T<T>& vec1, const CGeomVector2T<T>& vec2)
 {
 	return (vec1.x * vec2.y) - (vec1.y * vec2.x);
 }
 
-typedef Vector2T<float>  Vector2f;
-typedef Vector2T<double> Vector2d;
+typedef CGeomVector2T<float>  CGeomVector2f;
+typedef CGeomVector2T<double> CGeomVector2d;
 
-}
--- test/test.cpp
+++ test/test.cpp
@@ -3,36 +3,36 @@
 
 TEST(GeomVecter2fTest,AccessXY)
 {
-	Geom::Vector2f vec(1.0f, 2.0f);
+	CGeomVector2f vec(1.0f, 2.0f);
 	EXPECT_EQ(1.0f, vec.x);
 	EXPECT_EQ(2.0f, vec.y);
 }
 
 TEST(GeomVecter2fTest,AccessWithIndices)
 {
-	Geom::Vector2f vec(1.0f, 2.0f);
+	CGeomVector2f vec(1.0f, 2.0f);
 	EXPECT_EQ(1.0f, vec.v[0]);
 	EXPECT_EQ(2.0f, vec.v[1]);
 }
 
 TEST(GeomVecter2dTest,AccessXY)
 {
-	Geom::Vector2d vec(1.0, 2.0);
+	CGeomVector2d vec(1.0, 2.0);
 	EXPECT_EQ(1.0, vec.x);
 	EXPECT_EQ(2.0, vec.y);
 }
 
 TEST(GeomVecter2dTest,AccessWithIndices)
 {
-	Geom::Vector2d vec(1.0, 2.0);
+	CGeomVector2d vec(1.0, 2.0);
 	EXPECT_EQ(1.0, vec.v[0]);
 	EXPECT_EQ(2.0, vec.v[1]);
 }
 
 TEST(GeomVecter2dTest,AdditionAssignment)
 {
-	Geom::Vector2d vec1(1.0, 2.0);
-	Geom::Vector2d vec2(3.0, 5.0);
+	CGeomVector2d vec1(1.0, 2.0);
+	CGeomVector2d vec2(3.0, 5.0);
 	vec1 += vec2;
 	EXPECT_EQ(4.0, vec1.x);
 	EXPECT_EQ(7.0, vec1.y);
@@ -40,8 +40,8 @@ TEST(GeomVecter2dTest,AdditionAssignment)
 
 TEST(GeomVecter2dTest,SubtractionAssignment)
 {
-	Geom::Vector2d vec1(1.0, 2.0);
-	Geom::Vector2d vec2(3.0, 5.0);
+	CGeomVector2d vec1(1.0, 2.0);
+	CGeomVector2d vec2(3.0, 5.0);
 	vec2 -= vec1;
 	EXPECT_EQ(2.0, vec2.x);
 	EXPECT_EQ(3.0, vec2.y);
@@ -49,7 +49,7 @@ TEST(GeomVecter2dTest,SubtractionAssignment)
 
 TEST(GeomVecter2dTest,MultiplicationAssignment)
 {
-	Geom::Vector2d vec(1.0, 2.0);
+	CGeomVector2d vec(1.0, 2.0);
 	vec *= 1.5;
 	EXPECT_EQ(1.5, vec.x);
 	EXPECT_EQ(3.0, vec.y);
@@ -57,7 +57,7 @@ TEST(GeomVecter2dTest,MultiplicationAssignment)
 
 TEST(GeomVecter2dTest,DivisionAssignment)
 {
-	Geom::Vector2d vec(1.0, 2.0);
+	CGeomVector2d vec(1.0, 2.0);
 	vec /= 4.0;
 	EXPECT_EQ(0.25, vec.x);
 	EXPECT_EQ(0.5, vec.y);
@@ -65,118 +65,118 @@ TEST(GeomVecter2dTest,DivisionAssignment)
 
 TEST(GeomVecter2dTest,Addition)
 {
-	Geom::Vector2d vec1(1.0, 2.0);
-	Geom::Vector2d vec2(3.0, 5.0);
-	Geom::Vector2d result = vec1 + vec2;
+	CGeomVector2d vec1(1.0, 2.0);
+	CGeomVector2d vec2(3.0, 5.0);
+	CGeomVector2d result = vec1 + vec2;
 	EXPECT_EQ(4.0, result.x);
 	EXPECT_EQ(7.0, result.y);
 }
 
 TEST(GeomVecter2dTest,Subtraction)
 {
-	Geom::Vector2d vec1(1.0, 2.0);
-	Geom::Vector2d vec2(3.0, 5.0);
-	Geom::Vector2d result = vec2 - vec1;
+	CGeomVector2d vec1(1.0, 2.0);
+	CGeomVector2d vec2(3.0, 5.0);
+	CGeomVector2d result = vec2 - vec1;
 	EXPECT_EQ(2.0, result.x);
 	EXPECT_EQ(3.0, result.y);
 }
 
 TEST(GeomVecter2dTest,Multiplication)
 {
-	Geom::Vector2d vec(1.2, 2.0);
-	Geom::Vector2d result = vec * 2;
+	CGeomVector2d vec(1.2, 2.0);
+	CGeomVector2d result = vec * 2;
 	EXPECT_EQ(2.4, result.x);
 	EXPECT_EQ(4.0, result.y);
 }
 
 TEST(GeomVecter2dTest,Multiplication2)
 {
-	Geom::Vector2d vec(1.2, 2.0);
-	Geom::Vector2d result = 2 * vec;
+	CGeomVector2d vec(1.2, 2.0);
+	CGeomVector2d result = 2 * vec;
 	EXPECT_EQ(2.4, result.x);
 	EXPECT_EQ(4.0, result.y);
 }
 
 TEST(GeomVecter2dTest,Division)
 {
-	Geom::Vector2d vec(1.0, 2.0);
-	Geom::Vector2d result = vec / 4;
+	CGeomVector2d vec(1.0, 2.0);
+	CGeomVector2d result = vec / 4;
 	EXPECT_EQ(0.25, result.x);
 	EXPECT_EQ(0.5, result.y);
 }
 
 TEST(GeomVecter2dTest,DotProduct)
 {
-	Geom::Vector2d vec1(1.0, 2.0);
-	Geom::Vector2d vec2(3.0, 5.0);
+	CGeomVector2d vec1(1.0, 2.0);
+	CGeomVector2d vec2(3.0, 5.0);
 	EXPECT_EQ(13.0, DotProduct(vec1, vec2));
 }
 
 TEST(GeomVecter2dTest,CrossProduct2D)
 {
-	Geom::Vector2d vec1(2.0, 3.0);
-	Geom::Vector2d vec2(4.0, 5.0);
+	CGeomVector2d vec1(2.0, 3.0);
+	CGeomVector2d vec2(4.0, 5.0);
 	EXPECT_EQ(-2.0, CrossProduct2D(vec1, vec2));
 }
 
 TEST(GeomVecter2dTest,Length)
 {
-	Geom::Vector2d vec(-3.0, 4.0);
+	CGeomVector2d vec(-3.0, 4.0);
 	EXPECT_EQ(5.0, vec.Length());
 }
 
 TEST(GeomVecter2dTest,LengthSquared)
 {
-	Geom::Vector2d vec(-3.0, 4.0);
+	CGeomVector2d vec(-3.0, 4.0);
 	EXPECT_EQ(25.0, vec.LengthSquared());
 }
 
 TEST(GeomVecter2dTest,EqualityOperator)
 {
-	Geom::Vector2d vec1(-3.0, 4.0);
-	Geom::Vector2d vec2(-3.0, 4.0);
+	CGeomVector2d vec1(-3.0, 4.0);
+	CGeomVector2d vec2(-3.0, 4.0);
 	EXPECT_TRUE(vec1 == vec2);
 }
 
 TEST(GeomVecter2dTest,InequalityOperator1)
 {
-	Geom::Vector2d vec1(-3.0, 5.0);
-	Geom::Vector2d vec2(-3.0, 4.0);
+	CGeomVector2d vec1(-3.0, 5.0);
+	CGeomVector2d vec2(-3.0, 4.0);
 	EXPECT_TRUE(vec1 != vec2);
 }
 
 TEST(GeomVecter2dTest,InequalityOperator2)
 {
-	Geom::Vector2d vec1(3.0, 4.0);
-	Geom::Vector2d vec2(-3.0, 4.0);
+	CGeomVector2d vec1(3.0, 4.0);
+	CGeomVector2d vec2(-3.0, 4.0);
 	EXPECT_TRUE(vec1 != vec2);
 }
 
 TEST(GeomVecter2dTest,CopyConstructor)
 {
-	Geom::Vector2d vec1(3.0, 4.0);
-	Geom::Vector2d vec2(vec1);
+	CGeomVector2d vec1(3.0, 4.0);
+	CGeomVector2d vec2(vec1);
 	EXPECT_EQ(vec2, vec1);
 }
 
 TEST(GeomVecter2dTest,Assignment)
 {
-	Geom::Vector2d vec1(3.0, 4.0);
-	Geom::Vector2d vec2;
+	CGeomVector2d vec1(3.0, 4.0);
+	CGeomVector2d vec2;
 	vec2 = vec1;
 	EXPECT_EQ(vec2, vec1);
 }
 
 TEST(GeomVecter2dTest,UnaryPlusOperator)
 {
-	Geom::Vector2d vec(-3.0, 4.0);
+	CGeomVector2d vec(-3.0, 4.0);
 	EXPECT_EQ(vec, +vec);
 }
 
 TEST(GeomVecter2dTest,UnaryNegationOperator)
 {
-	Geom::Vector2d vec1(-3.0, 4.0);
-	Geom::Vector2d vec2(3.0, -4.0);
+	CGeomVector2d vec1(-3.0, 4.0);
+	CGeomVector2d vec2(3.0, -4.0);
 	EXPECT_EQ(vec2, -vec1);
 }
 
